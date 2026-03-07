bd399cadc7f050f7f4a11e057fd90554c0252147
Ivan Esensky <ivanesensky@gmail.com>
2026-03-08 02:37:44 +0300

    Initial project setup with SDL2

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
-int main(int argc, char* argv[]) {
+int SDL_main(int argc, char* argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "SDL could not initialize! Error: " << SDL_GetError() << std::endl;
         return 1;
