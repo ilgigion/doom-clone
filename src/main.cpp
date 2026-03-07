a8b731c67fc28fd0e428ff91c5716f111dbd131a
Ivan Esensky <ivanesensky@gmail.com>
2026-03-08 02:49:29 +0300

    Add basic window and game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,66 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
-int SDL_main(int argc, char* argv[]) {
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+int main(int argc, char* argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "SDL could not initialize! Error: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-    std::cout << "Project initialized successfully!" << std::endl;
+    SDL_Window* window = SDL_CreateWindow(
+        "Doom Clone",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        SDL_WINDOW_SHOWN
+    );
 
+    if (window == nullptr) {
+        std::cout << "Window could not be created! Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+    );
+
+    if (renderer == nullptr) {
+        std::cout << "Renderer could not be created! Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool quit = false;
+    SDL_Event e;
+
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+            if (e.type == SDL_KEYDOWN) {
+                switch (e.key.keysym.sym) {
+                    case SDLK_ESCAPE:
+                        quit = true;
+                        break;
+                }
+            }
+        }
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
+
     return 0;
 }
