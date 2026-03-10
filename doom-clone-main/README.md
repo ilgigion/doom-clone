468e55e5ca30d72eccac17e8d9547e9d1e4726bf
MOR689 <moryzhikov@edu.hse.ru>
2026-03-11 00:37:19 +0300

    Add files via upload

---
+++ doom-clone-main/README.md
@@ -0,0 +1,10 @@
+# Doom Clone
+
+Simple Doom-like engine written in C++ using SDL2.
+
+## Build Instructions
+
+1. Install SDL2
+2. Create build folder
+3. Run cmake ..
+4. Run make
---
+++ doom-clone-main/include/Player.h
@@ -0,0 +1,38 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "Entity.h"
+#include "Map.h"
+#include <SDL2/SDL.h>
+
+class Player : public Entity {
+private:
+    float velocity;
+    float maxSpeed;
+    float acceleration;
+    float deceleration;
+    float rotSpeed;
+    float fov;
+
+    bool moveForward;
+    bool moveBackward;
+    bool turnLeft;
+    bool turnRight;
+
+public:
+    Player(float startX, float startY);
+    ~Player();
+
+    // in future: moving player
+    void update(float deltaTime, const Map& map); 
+    // renderring
+    void render(class Renderer& renderer) override;
+    // keyboard input
+    void handleInput(const uint8_t* keyState);
+
+    // field of view
+    float getFov() const;
+    float getDir() const;
+};
+
+#endif
